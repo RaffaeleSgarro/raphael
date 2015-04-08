4535bb7995ca82953e5018e7f2856ffa1b73c50f
Raffaele Sgarro <raffaelesgarro@gmail.com>
2015-04-08 12:01:22 +0200

    Improved error reporting

--- code/server.js
+++ code/server.js
@@ -8,7 +8,16 @@ app.use(express.static(__dirname));
 app.get('/search', function(req, res){
   var core = req.query.core;
   var q = req.query.q;
-  request('http://localhost:8983/solr/' + core + '/select?q=' + q + '&wt=json&indent=true').pipe(res);
+  var requestUri = 'http://localhost:8983/solr/' + core + '/select?q=' + q + '&wt=json&indent=true';
+  request({
+    uri: requestUri,
+    timeout: 1000
+    })
+    .on('error', function(e){
+      console.log('Error while requesting ' + requestUri);
+      console.log(e);
+    })
+    .pipe(res);
 });
 
 app.listen(3333);
